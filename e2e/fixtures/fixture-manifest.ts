e4a622a7f211093c40fc5d4490ed12b46cd641d2
Evan Morgan <evan.morgan@inscope.sh>
2026-06-14 16:13:41 +0100

    feat(CT-114): add deterministic e2e fixture set with documented manifest

---
+++ e2e/fixtures/fixture-manifest.ts
@@ -0,0 +1,44 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import manifestJson from "./manifest.json" with { type: "json" };
+
+export interface FixtureSamplePixel {
+  readonly x: number;
+  readonly y: number;
+  readonly valuesPerBand: ReadonlyArray<number>;
+}
+
+export interface SingleFileFixture {
+  readonly fileName: string;
+  readonly width: number;
+  readonly height: number;
+  readonly bandCount: number;
+  readonly dataType: string;
+  readonly bandMeans?: ReadonlyArray<number>;
+  readonly samplePixels: ReadonlyArray<FixtureSamplePixel>;
+}
+
+export interface EnviFixture {
+  readonly headerFileName: string;
+  readonly binaryFileName: string;
+  readonly width: number;
+  readonly height: number;
+  readonly bandCount: number;
+  readonly dataType: string;
+  readonly wavelengths: ReadonlyArray<number>;
+  readonly bandMeans: ReadonlyArray<number>;
+  readonly samplePixels: ReadonlyArray<FixtureSamplePixel>;
+}
+
+const FIXTURES_DIRECTORY = dirname(fileURLToPath(import.meta.url));
+
+export const lowContrastGrayPng = manifestJson.lowContrastGrayPng as SingleFileFixture;
+export const rgbPng = manifestJson.rgbPng as SingleFileFixture;
+export const multiBandTiff = manifestJson.multiBandTiff as SingleFileFixture;
+export const flatFieldReferenceTiff = manifestJson.flatFieldReferenceTiff as SingleFileFixture;
+export const enviStack = manifestJson.enviStack as EnviFixture;
+
+export function fixturePath(fileName: string): string {
+  return join(FIXTURES_DIRECTORY, fileName);
+}
